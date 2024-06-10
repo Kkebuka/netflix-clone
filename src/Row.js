da4ca83fd9504ba0799669846a6e4cba5eb827ec
kkebuka <kkebuka@gmail.com>
2024-06-10 08:56:01 +0100

    update to app,js and row.js

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_Url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -17,6 +20,32 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
 
+  const opts = {
+    height: "300",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      // console.log("movie name", movie.name);
+      // console.log("movie name", movie.title);
+      movieTrailer(movie?.title || movie?.name || "")
+        .then((url) => {
+          console.log("movie url", url);
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+
+        .catch((error) => console.log(`Nawa ${error}`));
+    }
+  };
+
   return (
     <div className="row">
       {/* title */}
@@ -26,6 +55,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movies.id}
+            onClick={() => handleClick(movie)}
             className={`row__posters ${isLargeRow && "row__posterLarge"}`}
             src={`${base_Url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,7 +65,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         ))}
       </div>
 
-      {/* Container -> posters */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
